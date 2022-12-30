5576730f00108f0e2582bd3a14891af3d303289d
Shota-Kurahashi <Myheroes0715@gmail.com>
2022-12-30 17:18:42 +0900

    Mainページの読み込み

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,14 @@
+import { QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import React from "react";
+import { Main } from "./components/Main";
+import { queryClient } from "./libs/QueryClient";
 
-const App = () => <div />;
+const App = () => (
+  <QueryClientProvider client={queryClient}>
+    <Main />
+    <ReactQueryDevtools initialIsOpen={false} />
+  </QueryClientProvider>
+);
 
 export default App;
